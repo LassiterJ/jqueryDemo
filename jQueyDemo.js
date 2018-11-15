0263c87ec37a9107c53b32855fece29b053de02f
Jonathan Lassiter <36551695+LassiterJ@users.noreply.github.com>
2018-11-14 20:46:11 -0500

    Add files via upload

---
+++ jQueyDemo.js
@@ -0,0 +1,8 @@
+// select all divs and give them a purple background
+$("div").css("background", "purple");
+// select the divs with the class "highlight" and make width 200px
+$("div.highlight").css("width", "200px");
+// select the divs with id "third" and give it an orange border
+$("#third").css("border", "2px solid orange");
+// Bonus: select the first div only and change the text to pink
+$("div:first-of-type").css("color", "pink");
